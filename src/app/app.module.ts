1b5cec0a2f2d1c008308c6e4031ea08b620d77b9
J.Edu <jedu.f.dantas@gmail.com>
2022-03-09 14:14:45 -0300

    Testing first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyHomeComponent } from './my-home/my-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent
   ],
   imports: [
     BrowserModule
